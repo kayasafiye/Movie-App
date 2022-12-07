76a263310d164ebb5fab70741f089885c769260a
Safiye KAYA <87526029+kayasafiye@users.noreply.github.com>
2022-12-07 17:11:20 +0300

    MovieApplication
    Basit Film Sayfası Tasarımı

---
+++ swiper.js
@@ -0,0 +1,36 @@
+// Initialize Swiper
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: 
+      {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay:
+      {
+        delay:5000,
+        disableOnInteraction:false,
+
+      },
+      navigation: 
+      {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+    });
